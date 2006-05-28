46fff48dfb3dd6a2b4c13fa438c0224a524c48fb
kevinc <kevinc@fc021c06-de14-0410-8c5a-e664d767796e>
2006-05-28 23:29:46 +0000

    Add special case for replace_html. Clean up usage

--- lib/arts.rb
+++ lib/arts.rb
@@ -7,16 +7,32 @@ module Arts
   include ActionView::Helpers::TagHelper
   
   def assert_rjs(action, *args, &block)
-    return assert_rjs_insert_html(action, *args) if action.to_sym == :insert_html
-    assert lined_response.include?(create_generator.send(action, *args, &block)), "#{action} with args [#{args.join(" ")}] does not show up in response"
+    case action.to_sym
+      when :insert_html
+        return assert_rjs_insert_html(*args)
+      when :replace_html
+        return assert_rjs_replace_html(*args)
+      else
+        assert lined_response.include?(create_generator.send(action, *args, &block)), 
+               generic_error(action, args)
+    end
   end
   
-  def assert_rjs_insert_html(action, *args)
+  def assert_rjs_insert_html(*args)
     position = args.shift
     item_id = args.shift
     content = args.shift
 
-    assert lined_response.include?("new Insertion.#{position.to_s.camelize}(\"#{item_id}\", \"#{content}\");")
+    assert lined_response.include?("new Insertion.#{position.to_s.camelize}(\"#{item_id}\", \"#{content}\");"),
+           generic_error(action, original_args)
+  end
+  
+  def assert_rjs_replace_html(*args)
+    div = args.shift
+    content = args.shift
+    
+    assert lined_response.include?("Element.update(\"#{div}\", \"#{content}\");"), 
+           "No replace_html call found on div: '#{div}' and content: '#{content}'"
   end
   
   protected
@@ -28,5 +44,9 @@ module Arts
   def create_generator
     block = Proc.new { |*args| yield *args if block_given? } 
     JavaScriptGenerator.new self, &block
+  end
+  
+  def generic_error(action, args)
+    "#{action} with args [#{args.join(" ")}] does not show up in response"
   end
 end
